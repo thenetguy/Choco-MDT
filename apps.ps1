ef104db723861255699164590d73ccee4dde1f4f
thenetguy <thenetguy@gmail.com>
2019-10-18 13:44:27 -0400

    Update apps.ps1

--- apps.ps1
+++ apps.ps1
@@ -1,13 +1,27 @@
-﻿#import MicrosoftDeploymentToolkit.psd1
-Write-Host -ForegroundColor Green -BackgroundColor black "importing MicrosoftDeploymentToolkit.psd1..."
+﻿Write-Host -ForegroundColor Green -BackgroundColor Black "checking if Microsoft Deployment Toolkit is installed ..."
+Start-Sleep -Seconds 5
+$CheckMDT= Test-Path "C:\Program Files\Microsoft Deployment Toolkit\bin\MicrosoftDeploymentToolkit.psd1"
+if ($CheckMDT)
+{
+Write-Host -ForegroundColor Green -BackgroundColor Black "Microsoft Deployment Toolkitmust is installed, lets continue"
+Start-Sleep -Seconds 5
+}  
+else
+{
+
+Write-Host -ForegroundColor Yellow -BackgroundColor Black "Microsoft Deployment Toolkitmust is not installed. Please install MDT and run this script again"
+exit
+}
+#import MicrosoftDeploymentToolkit.psd1
+Write-Host -ForegroundColor Green -BackgroundColor Black "importing MicrosoftDeploymentToolkit.psd1..."
 Start-Sleep -Seconds 5
 Import-Module "C:\Program Files\Microsoft Deployment Toolkit\bin\MicrosoftDeploymentToolkit.psd1"
 #Create choco folder under applications in MDT
-Write-Host -ForegroundColor Green -BackgroundColor black "Creating choco folder under applications in MDT"
+Write-Host -ForegroundColor Green -BackgroundColor Black "Creating choco folder under applications in MDT"
 Start-Sleep -Seconds 5
 new-item -path "DS001:\Applications" -enable "True" -Name "Choco" -Comments "" -ItemType "folder" -Verbose
 #import app list from apps.txt *muser be located in the same folder as this script*
-Write-Host -ForegroundColor Green -BackgroundColor black "importing app list from apps.txt into MDT"
+Write-Host -ForegroundColor Green -BackgroundColor Black "importing app list from apps.txt into MDT"
 Start-Sleep -Seconds 5
 $Apps = Get-Content -Path .\apps.txt
 #add apps from apps.txt to MDT
@@ -15,4 +29,4 @@ ForEach($Apps In $Apps)
 {
 import-MDTApplication -path "DS001:\Applications\Choco" -enable "True" -Name "$Apps" -ShortName "$Apps" -Version "" -Publisher "" -Language "" -CommandLine "powershell.exe choco install $Apps -Y --Force" -WorkingDirectory "" -NoSource -Verbose
 }
-pause
+Start-Sleep -Seconds 30
