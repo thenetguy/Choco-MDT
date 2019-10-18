024a9bf438a618dec8313af1304eae0d9a8fb293
thenetguy <thenetguy@gmail.com>
2019-10-18 13:15:07 -0400

    Update apps.ps1

--- apps.ps1
+++ apps.ps1
@@ -1,13 +1,18 @@
-﻿Import-Module "C:\Program Files\Microsoft Deployment Toolkit\bin\MicrosoftDeploymentToolkit.psd1"
-New-PSDrive -Name "DS001" -PSProvider MDTProvider -Root "C:\DeploymentShare"
+﻿#import MicrosoftDeploymentToolkit.psd1
+Write-Host -ForegroundColor Green -BackgroundColor black "importing MicrosoftDeploymentToolkit.psd1..."
+Start-Sleep -Seconds 5
+Import-Module "C:\Program Files\Microsoft Deployment Toolkit\bin\MicrosoftDeploymentToolkit.psd1"
+#Create choco folder under applications in MDT
+Write-Host -ForegroundColor Green -BackgroundColor black "Creating choco folder under applications in MDT"
+Start-Sleep -Seconds 5
 new-item -path "DS001:\Applications" -enable "True" -Name "Choco" -Comments "" -ItemType "folder" -Verbose
-
+#import app list from apps.txt *muser be located in the same folder as this script*
+Write-Host -ForegroundColor Green -BackgroundColor black "importing app list from apps.txt into MDT"
+Start-Sleep -Seconds 5
 $Apps = Get-Content -Path .\apps.txt
-
+#add apps from apps.txt to MDT
 ForEach($Apps In $Apps)
-
 {
 import-MDTApplication -path "DS001:\Applications\Choco" -enable "True" -Name "$Apps" -ShortName "$Apps" -Version "" -Publisher "" -Language "" -CommandLine "powershell.exe choco install $Apps -Y --Force" -WorkingDirectory "" -NoSource -Verbose
 }
-
 pause
