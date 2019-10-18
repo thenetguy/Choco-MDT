20c684a358493310f226868522249a4436251237
thenetguy <thenetguy@gmail.com>
2019-10-18 09:58:50 -0400

    initial commit

---
+++ CheckChoco.ps1
@@ -0,0 +1,18 @@
+﻿Write-Host -ForegroundColor Green -BackgroundColor black "checking if C:\ProgramData\chocolatey\choco.exe is installed"
+Start-Sleep -Seconds 5
+$Checkchoco= Test-Path C:\ProgramData\chocolatey\choco.exe
+if ($Checkchoco)
+{
+Write-Host -ForegroundColor yellow -BackgroundColor black "Choco is already installed. exiting..."
+Start-Sleep -Seconds 5
+exit
+}  
+else
+{
+Write-Host -ForegroundColor Green -BackgroundColor black "Choco is not installed, Installing Choco..."
+Start-Sleep -Seconds 5
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+Write-Host -ForegroundColor Green -BackgroundColor black "Choco Has been installed on this system. exiting"
+Start-Sleep -Seconds 10
+}
---
+++ apps.ps1
@@ -0,0 +1,13 @@
+﻿Import-Module "C:\Program Files\Microsoft Deployment Toolkit\bin\MicrosoftDeploymentToolkit.psd1"
+New-PSDrive -Name "DS001" -PSProvider MDTProvider -Root "C:\DeploymentShare"
+new-item -path "DS001:\Applications" -enable "True" -Name "Choco" -Comments "" -ItemType "folder" -Verbose
+
+$Apps = Get-Content -Path .\apps.txt
+
+ForEach($Apps In $Apps)
+
+{
+import-MDTApplication -path "DS001:\Applications\Choco" -enable "True" -Name "$Apps" -ShortName "$Apps" -Version "" -Publisher "" -Language "" -CommandLine "powershell.exe choco install $Apps -Y --Force" -WorkingDirectory "" -NoSource -Verbose
+}
+
+pause
